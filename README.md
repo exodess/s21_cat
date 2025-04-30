35538bedbac3bd89251646fa65290754096a99fc
Georgiy <goshakovalev5@gmail.com>
2025-04-30 14:05:33 +0400

    Readme file with project description and links

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
 # s21_cat - new version of the cat utility
 Developed by versiese for the SimpleBash project at School21
 
-### Installation
+## Installation
 1. Clone this repository
-	#### `git clone https://github.com/exodess/s21_cat`
+#### `git clone https://github.com/exodess/s21_cat`
 2. Go to the project folder
-	#### `cd s21_cat/`
+#### `cd s21_cat/`
 3. Build the project using make
-	#### `make`
+#### `make`
 
-### Testing
+## Testing
 
 There is a bash script in the project folder that can be used to compare the operation of s21_cat with the cat command
 
 Running the test:
-	#### `make test`
+#### `make test`
 
 
-### System information
+## System information
 
 Version: 1.0
-Developed by 
-##### t.me/@exodesk
+Developed by [exodes](t.me/@exodesk)
---
+++ tools/test_cat.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+clear
+echo "Запуск тестирования новой утилиты s21_cat..."
+read -p "Введите файл, на котором будет происходить тестирование: " test_file
+
+test_file=$(echo $test_file | cut -f 1 -d ' ')
+
+if [ -e $test_file ]
+	then
+		echo ""
+	else
+		echo "Такого файла не существует, программа завершает свою работу..."
+		exit 0
+fi
+
+file_s21="out21cat.txt"
+file_cat="outcat.txt"
+
+n=1
+succ=0
+fail=0
+for flag in "b" "e" "n" "s" "t" "v" "sE" "T" "veb" "enssv" "ETsvb" "sbentvET"
+	do 
+		echo -n "TEST $n. Flag = $flag : "
+		./s21_cat -$flag $test_file > $file_s21 
+		cat -$flag $test_file > $file_cat
+
+		result=$(diff $file_s21 $file_cat)
+		len=${#result}
+		if [[ len -eq 0 ]]
+			then 
+				echo -e  "\033[32m" SUCCESFULL" \033[0m"
+				succ=$((succ + 1))
+			else 
+				echo -e "\033[31m" "   FAIL" "\033[0m"
+				fail=$((fail + 1))
+		fi
+		
+		# echo $result
+		n=$((n + 1))
+	done
+
+percent=$(($((succ * 100)) / $((succ + fail))))
+echo ""
+echo "Успехов: $succ, неудач: $fail"
+echo "Прохождение теста: $percent%"
+
+rm $file_s21
+rm $file_cat
+echo "Тесты завершены!"
+exit 0 
