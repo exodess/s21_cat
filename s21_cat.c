6f9704c1bd9a02b5c1178402274fa8c40adaa3d0
Georgiy <goshakovalev5@gmail.com>
2025-05-04 18:44:43 +0400

    Main file with s21_cat utility code

--- s21_cat.c
+++ s21_cat.c
@@ -3,10 +3,10 @@
 #include <getopt.h>
 #include <stdlib.h>
 #include "s21_cat_flags.h"
-#include "../common/list.h"
-// #include "../common/colors.h" // окрашивание вывода
+#include "tools/list.h"
 
-#define _GNU_SOURCE
+#define _GNU_SOURCE 
+#define MODEFL(ch) modes[strchr(FLAGS, *#ch) - FLAGS] // просмотр значения конкретного флага в массиве modes
 
 void processing_modes(int modes[COUNT_GNU_FLAGS]);
 void processing_input(int argc, char * argv[], 
@@ -31,16 +31,6 @@ int main(int argc, char *argv[])
 	
 	processing_input(argc, argv, long_options, modes, &list_file);
 	processing_modes(modes);
-	
-	if(list_file -> name == NULL) 
-		push(&list_file, "-");
-	
-// 	printf("|b|E|n|s|T|v|\n");
-// 	for(int i = 0; i < COUNT_GNU_FLAGS; i++) printf("|%d", modes[i]);
-// 	puts("|");
-// 
-// 	printList(list_file);
-
 
 	while(list_file -> name != NULL) {
 		if(strcmp(list_file -> name, "-") == 0) fp = stdin;
@@ -50,14 +40,13 @@ int main(int argc, char *argv[])
 			while((here = fgetc(fp)) != EOF) {
 				res = 1;
 				// очередность вызова функций влияет на работоспособность программы!
-				if(modes[strchr(FLAGS, 'b') - FLAGS]) flag_b(&count, left, here);
-				// if(modes[strchr(FLAGS, 'n') - FLAGS]) flag_n(&count, left);
-		
-				if(modes[strchr(FLAGS, 'T') - FLAGS]) res = flag_T(here);
-				if(modes[strchr(FLAGS, 'v') - FLAGS]) res = res && flag_v(here);
-				if(modes[strchr(FLAGS, 's') - FLAGS]) res = res && flag_s(left, here, &flag);
-				if(modes[strchr(FLAGS, 'n') - FLAGS] && res) flag_n(&count, left);
-				if(modes[strchr(FLAGS, 'E') - FLAGS] && res) flag_E(here);
+				if(MODEFL(b)) flag_b(&count, left, here);
+				if(MODEFL(T)) res = flag_T(here);
+				if(MODEFL(v)) res = res && flag_v(here);
+				if(MODEFL(s)) res = res && flag_s(left, here, &flag);
+				if(MODEFL(n) && res) flag_n(&count, left);
+				if(MODEFL(E) && res) flag_E(here);
+
 				if(res) printf("%c", here);
 				left = here;
 			}
@@ -80,46 +69,42 @@ void processing_input(int argc, char * argv[],
 					struct option long_options[COUNT_GNU_FLAGS], int modes[COUNT_GNU_FLAGS],
 					LIST ** root)
 {	
-	int rez = 0, j = 0;
-	int * list_index = malloc(argc * sizeof(int));
-
-	for(int i = 0; i < argc; i++) list_index[i] = -1;
+	int rez = 0;
+	int opt_index;
 
 	for(int i = 0; i < COUNT_GNU_FLAGS; i++) {
 		long_options[i].name = GNU_FLAGS[i];
 		long_options[i].has_arg = no_argument;
 		long_options[i].flag = &modes[i];
-		long_options[i].val = 0;
+		long_options[i].val = 1;
 	}
 	long_options[COUNT_GNU_FLAGS].name = NULL;
 	long_options[COUNT_GNU_FLAGS].has_arg = 0;
 	long_options[COUNT_GNU_FLAGS].flag = NULL;
 	long_options[COUNT_GNU_FLAGS].val = 0;
 
-	while((rez = getopt_long(argc, argv, FLAGS, long_options, &list_index[j])) != -1) {
+	while((rez = getopt_long(argc, argv, FLAGS, long_options, &opt_index)) != -1) {
 		if(rez) { // короткое название
 			for(int i = 0; i < COUNT_GNU_FLAGS; i++)
 				modes[i] += (modes[i]) ? 0 : (rez == FLAGS[i]);	
-			if(rez == 'e') 
-				modes[strchr(FLAGS, 'E') - FLAGS] = modes[strchr(FLAGS, 'v') - FLAGS] = 1;
-			if(rez == 't')
-				modes[strchr(FLAGS, 'T') - FLAGS] = modes[strchr(FLAGS, 'v') - FLAGS] = 1;
 
+			if(rez == 'e') MODEFL(E) = MODEFL(v) = 1;
+			if(rez == 't') MODEFL(T) = MODEFL(v) = 1;
 		}
-		j++;
 	}	
 
 	for(int i = (argc - 1); i > 0; i--) 
 		if((argv[i][0] == '-' && strlen(argv[i]) == 1) || argv[i][0] != '-') 
 			push(root, argv[i]);
-	
+
+	if((*root) -> name == NULL) 
+		push(root, "-");
 }
 
 
 void processing_modes(int modes[COUNT_GNU_FLAGS])
 {
 	// -bn -> -b
-	if(modes[strchr(FLAGS, 'b') - FLAGS]) modes[strchr(FLAGS, 'n') - FLAGS] = 0;
-
-	
+	if(MODEFL(b)) 
+		MODEFL(n) = 0;
 }
